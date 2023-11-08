e53e662c4caecec2ff0a98936ea8ad28b42af7b9
Giuseppe Sansone <giuseppesansone144@gmail.com>
2023-11-08 17:48:36 +0100

    done

---
+++ pratica giorno 8/js/javas.js
@@ -0,0 +1,7 @@
+/*const paintItGreen = function () {
+        let background = document.querySelectorAll("ul")
+        for(let i = 0; i < background.length; i++){
+          background[i].style.backgroundColor = "green"
+        }
+      }
+      paintItGreen()*/
